0aa49d290bac9510d7332708a62788e77798efb0
arif481 <mdarifuzzaman481@gmail.com>
2026-01-16 20:51:30 +0530

    Fix build errors: component exports and ESLint config

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,6 +7,8 @@ export * from './Card';
 export * from './Input';
 export * from './Modal';
 export * from './Progress';
+export { ProgressBar as Progress } from './Progress';
 export * from './Badge';
 export * from './Avatar';
 export * from './Loading';
+export { Spinner as Loading } from './Loading';
